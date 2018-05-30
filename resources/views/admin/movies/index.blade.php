0bcf999bd39fddb9cc359a300350ddfbf0b03e71
Mikhaela Maun <gncrnmn@gmail.com>
2018-05-30 18:08:52 +0800

    added table not finished

--- resources/views/admin/movies/index.blade.php
+++ resources/views/admin/movies/index.blade.php
@@ -9,4 +9,45 @@
                 Add Movie
         </a>
 
+
+        <table id="movies-table" class="table table-bordered table-striped" style="width: 100%">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Last Name</th>
+                <th>First Name</th>
+                <th>Middle Name</th>
+                <th>Email</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($movies as $movie)
+              <tr>
+                <td>{{$movie->id}}</td>
+                <td>{{$movie->lastName}}</td>
+                <td>{{$movie->firstName}}</td>
+                <td>{{$movie->middleName}}</td>
+                <td>{{$movie->email}}</td>
+                <td>{{$movie->created_at}}</td>
+                <td>{{$movie->updated_at}}</td>
+              </tr>
+              @endforeach
+            </tbody>
+        </table>
+
+@endsection
+
+@section('script')
+        <script type="text/javascript">
+                $(document).ready(function() {
+                    $('#movies-table').DataTable( {
+                        dom: 'Bfrtip',
+                        buttons: [
+                            'copy', 'csv', 'excel', 'pdf', 'print'
+                        ]
+                    } );
+                } );
+        </script>
 @endsection
